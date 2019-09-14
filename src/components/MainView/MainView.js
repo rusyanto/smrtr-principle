1d0b0f422af6ff470d57ab233c28c264cc515fb3
Rusyanto Rusanto <rusyanto@gmail.com>
2019-09-14 22:17:56 +0800

    Set up main grids

--- src/components/MainView/MainView.js
+++ src/components/MainView/MainView.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import './MainView.css';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary
+  root: {
+    flexGrow: 1,
+  },
+  header: {
+    fontWeight: 600,
+    padding: theme.spacing(1)
+  },
+  colLeft: {
+    float: 'left',
+    padding: theme.spacing(1)
+  },
+  colRight: {
+    width: '100%',
+    padding: theme.spacing(1)
+  },
+  row: {
+    '&::after': {
+      display: 'table',
+      clear: 'both'
     }
-  }));
+  }
+}));
 
 function MainView() {
     const classes = useStyles();
@@ -22,16 +36,44 @@ function MainView() {
         <div className={classes.root}>
           <Grid container spacing={3}>
             <Grid item xs={12} sm={7}>
-              <div className={classes.paper}>xs=12 sm=7</div>
+              <Grid className={classes.header} item xs={12}>
+                All Agents Stats
+              </Grid>
+              <Grid className={classes.row} item xs={12}>
+                <div className={classes.colLeft}>Test1</div>
+                <div className={classes.colRight}>Test2</div>
+              </Grid>
+              <Grid className={classes.row} item xs={12}>
+                <div className={classes.colLeft}>Test1</div>
+                <div className={classes.colRight}>Test2</div>
+              </Grid>
+              <Grid className={classes.row} item xs={12}>
+                <div className={classes.colLeft}>Test1</div>
+                <div className={classes.colRight}>Test2</div>
+              </Grid>
             </Grid>
             <Grid item xs={12} sm={5}>
-              <div className={classes.paper}>xs=12 sm=5</div>
+              <Grid className={classes.header} item xs={12}>
+                Local Stock
+              </Grid>
+              <Grid className={classes.row} item xs={12}>
+                <div className={classes.colLeft}>Test1</div>
+                <div className={classes.colRight}>Test2</div>
+              </Grid>
+              <Grid className={classes.row} item xs={12}>
+                <div className={classes.colLeft}>Test1</div>
+                <div className={classes.colRight}>Test2</div>
+              </Grid>
+              <Grid className={classes.row} item xs={12}>
+                <div className={classes.colLeft}>Test1</div>
+                <div className={classes.colRight}>Test2</div>
+              </Grid>
             </Grid>
             <Grid item xs={12} sm={5}>
-              <div className={classes.paper}>xs=12 sm=5</div>
+              <div className={classes.header}>xs=12 sm=5</div>
             </Grid>
             <Grid item xs={12} sm={7}>
-              <div className={classes.paper}>xs=12 sm=7</div>
+              <div className={classes.header}>xs=12 sm=7</div>
             </Grid>
           </Grid>
         </div>
